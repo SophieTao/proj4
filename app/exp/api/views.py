f83134cb4b5885eab2b6888f98ce78f220846062
Sophie <sophietaoyn@gmail.com>
2017-02-25 13:19:28 +0000

    modified exp

--- app/exp/api/views.py
+++ app/exp/api/views.py
@@ -3,15 +3,3 @@ from django.shortcuts import render
 import urllib.request
 import urllib.parse
 import json
-
-models_endpoint = 'http://models-api:8002/'
-
-# ITEMS
-def meals(request):
-	json = request_get('api/v1/meals')
-	return JsonResponse(json)
-
-def request_get(endpoint):
-	req = urllib.request.Request(models_endpoint + endpoint)
-	raw = urllib.request.urlopen(req).read().decode('utf-8')
-	return json.loads(raw)
