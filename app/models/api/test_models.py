43ac6e688621a3da43b769a5a2c986ca15ed7f35
Sophie <sophietaoyn@gmail.com>
2017-02-27 17:42:16 +0000

    removed dining

--- app/models/api/test_models.py
+++ app/models/api/test_models.py
@@ -1,7 +1,10 @@
 from django.test import TestCase, Client    
 from django.core.urlresolvers import reverse
-from cafes import models
+from api import models
+from django.core.management import call_command
+from .models import Cafe, Comment, Profile
 
+import json
 
 class GetOrderDetailsTestCase(TestCase):
         #setUp method is called before each test in this class
@@ -13,14 +16,12 @@ class GetOrderDetailsTestCase(TestCase):
                 description='test',
                 Calories = '300',
             )
-        def test_cafe_list(self):
+        def test_meal_list(self):
             response = self.client.get(reverse('cafe_list'))
-            resp_json = json.loads((response.content).decode("utf-8"))
-            print(response)
-            print(resp_json)
+
             self.assertEqual(response.status_code, 200)
         
-        def test_cafe_detail(self):
+        def test_meal_detail(self):
             url = reverse('meal_detail', args=[self.test_cafe.id])
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
@@ -29,8 +30,8 @@ class GetOrderDetailsTestCase(TestCase):
             response = self.client.get('/cafe/')
             self.assertEquals(response.status_code, 404)
 
-        def test_get_task_info_not_present(self):
-            response = self.client.get(reverse('cafe_list', args=[10]))
+        def fails_invalid_meal(self):
+            response = self.client.get(reverse('meal_detail', args=[10]))
             resp_json = (response.content).decode("utf-8")
             self.assertEquals(resp_json, 'False')
 
