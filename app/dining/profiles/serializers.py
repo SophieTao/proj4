85ca92a6a5c37db9429af3c764ede41bf1eb0c9d
Sophie <sophietaoyn@gmail.com>
2017-02-27 17:43:55 +0000

    removed dining

--- app/dining/profiles/serializers.py
+++
@@ -1,9 +0,0 @@
-# from rest_framework import serializers
-# from .models import profile
-
-# class ProfileSerializer(serializers.ModelSerializer):
-
-# 	class Meta:
-# 		model = profile
-# 		#fields = ('name')
-# 		fields = '__all__'
